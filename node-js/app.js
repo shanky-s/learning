43f9562ecf19da7d03b5f3070e4c2308f95b1653
Shashank Shekher <shashank.spinns@gmail.com>
2019-08-28 20:18:20 +0530

    latest updates and clean up

--- node-js/app.js
+++ node-js/app.js
@@ -1,11 +1,11 @@
-// console.log("Hey Shashank!")
-// var time = 0;
-// setInterval(function(){
-//     console.log( "time passed: "+ time++ + " secs" )
-// }, 1000)
-const event  = require('events');
+console.log("Hey Shashank!")
+var time = 0;
+setInterval(function(){
+    console.log( "time passed: "+ time++ + " secs" )
+}, 1000)
+// const event  = require('events');
 
-const fs  = require('fs');
+// const fs  = require('fs');
 
-const util = require('util');
+// const util = require('util');
 
---
+++ node-js/objectives.md
@@ -0,0 +1,4 @@
+# Intro To Node
+* What node is?
+* Why are we learning it?
+* (It does not matter)
